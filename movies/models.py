0a365d92212f1d6be4967e6ec3c460c63d63368d
Clement Odeh <odehclementonwu@gmail.com>
2025-12-04 18:18:52 +0100

    Add models to various apps

---
+++ movies/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.name
+
+class Movie(models.Model):
+    title = models.CharField(max_length = 200)
+    description = models.TextField()
+    category = models.ForeignKey(Genre, on_delete = models.SET_NULL, null = True)
+    release_year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
---
+++ subscriptions/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from datetime import timedelta
+
+# Create your models here.
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    status = models.CharField(max_length = 20, default = 'active')
+    start_date = models.DateTimeField(default = timezone.now)
+    end_date = models.DateTimeField(default = timezone.now() + timedelta(days=30))
+    last_payment_date = models.DateTimeField(default = timezone.now)
+
+    def is_active(self):
+        return self.end_date >= timezone.now()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.status}"
