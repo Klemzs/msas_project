3b3a25cb9efe65cda826c200fecbbf7d9cda0f56
Clement Odeh <odehclementonwu@gmail.com>
2025-12-12 07:23:48 +0100

    add models

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -4,11 +4,15 @@ from django.utils import timezone
 from datetime import timedelta
 
 # Create your models here.
+
+def default_end_date():
+    return timezone.now() + timedelta(days=30)
+
 class Subscription(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE)
     status = models.CharField(max_length = 20, default = 'active')
     start_date = models.DateTimeField(default = timezone.now)
-    end_date = models.DateTimeField(default = timezone.now() + timedelta(days=30))
+    end_date = models.DateTimeField(default = default_end_date)
     last_payment_date = models.DateTimeField(default = timezone.now)
 
     def is_active(self):
