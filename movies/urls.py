dc84ef28bb370566d9087ef20638c14bcac35d7d
Clement Odeh <odehclementonwu@gmail.com>
2025-12-15 18:36:18 +0100

    add urls

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path 
-from .views import MovieListView, GenreListView, MovieSearchView
+from .views import MovieListView, GenreListView, MovieSearchView, WatchMovieView
 
 urlpatterns = [
         path('', MovieListView.as_view(), name = 'movie-list'),
         path('search/', MovieSearchView.as_view(), name = 'movie-search'),
         path('genres/', GenreListView.as_view(), name = 'genre-list'),
+        path('<int:pk>/watch/', WatchMovieView.as_view(), name = 'movie-watch'),
     ]
