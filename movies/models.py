f3b009c9f7ea520c24ad1e639882e03e02deeb56
Clement Odeh <odehclementonwu@gmail.com>
2025-12-15 17:15:25 +0100

    add models

--- movies/models.py
+++ movies/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Genre(models.Model):
-    name = models.CharField(max_length = 100)
+    name = models.CharField(max_length = 100, unique = True)
 
     def __str__(self):
         return self.name
 
 class Movie(models.Model):
-    title = models.CharField(max_length = 200)
+    title = models.CharField(max_length = 255)
     description = models.TextField()
-    category = models.ForeignKey(Genre, on_delete = models.SET_NULL, null = True)
-    release_year = models.IntegerField()
+    genre = models.ForeignKey(Genre, on_delete = models.PROTECT, related_name = 'movies')
+    release_year = models.PositiveIntegerField()
 
     def __str__(self):
         return self.title
