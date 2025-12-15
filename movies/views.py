3d6d2ef3c786566f26aca6c6b13764165d258480
Clement Odeh <odehclementonwu@gmail.com>
2025-12-15 17:17:51 +0100

    add views

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Movie, Genre
+from django.db.models import Q
+from .serializers import MovieSerializer, GenreSerializer
 
-# Create your views here.
+class MovieListView(generics.ListAPIView):
+    queryset = Movie.objects.select_related('genre').all()
+    serializer_class = MovieSerializer
+    permission_classes = [permissions.AllowAny]
+
+class GenreListView(generics.ListAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = [permissions.AllowAny]
+
+class MovieSearchVIew(generics.ListAPIView):
+    serializer_class = MovieSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        query = self.request.query_params.get('q', '')
+        return Movie.objects.select_related('genre').filter(
+                Q(title__icontains = query) |
+                Q(description__icontains = query)
+        )
