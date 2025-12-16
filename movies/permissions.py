6d52acb5e810468377784218ed8d32c365fdd144
Clement Odeh <odehclementonwu@gmail.com>
2025-12-16 12:37:02 +0100

    update permission

--- movies/permissions.py
+++ movies/permissions.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 from django.utils import timezone
 from subscriptions.models import Subscription
 
@@ -15,3 +15,10 @@ class IsSubscribedUser(BasePermission):
                 user = user,
                 end_date__gte = timezone.now()
         ).exists()
+
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user and request.user.is_staff
+
